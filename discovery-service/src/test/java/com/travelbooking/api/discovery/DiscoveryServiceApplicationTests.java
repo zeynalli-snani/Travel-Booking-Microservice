5e3a688921051fb4c4cff00d921476e8e82548c8
iamnotsnani <zeynallisnani@gmail.com>
2026-05-18 21:39:33 +0400

    project structure created; Eureka server added

---
+++ discovery-service/src/test/java/com/travelbooking/api/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbooking.api.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ flight-service/src/test/java/com/travelbooking/api/flight/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbooking.api.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
