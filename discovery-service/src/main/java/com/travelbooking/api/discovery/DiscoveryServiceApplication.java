a24c656d691827a3ea6b724bd03e905bc2586459
iamnotsnani <zeynallisnani@gmail.com>
2026-05-19 16:48:03 +0400

    Bug with api gateway fixed, spring, java downgraded, f this zulu

--- discovery-service/src/main/java/com/travelbooking/api/discovery/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/travelbooking/api/discovery/DiscoveryServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
+        System.out.println("Current Java Version: " + System.getProperty("java.version"));
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
